4be7819bf0fe92a6f8ad36bbe6f8747f0ea52b22
kross97 <kross78080@yandex.ru>
2021-01-21 18:44:20 +0300

    Add theme global and storybook preview

--- basketball/.storybook/preview.js
+++ basketball/.storybook/preview.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import { theme } from "../src/themes/theme";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: Avenir;
+    src: url("../src/static/fonts/Avenir-Medium.ttf") format("truetype");
+  }
+  
+  * {
+    font-family: Avenir, sans-serif;
+  }
+`;
 
 export const decorators = [
     (Story) => (
         <ThemeProvider theme={theme}>
+            <GlobalStyle />
             <Story />
         </ThemeProvider>
     ),
--- basketball/src/themes/theme.js
+++ basketball/src/themes/theme.js
@@ -1,22 +1,23 @@
 export const theme = {
-    colors: {
-        blue: '#344472',
-        lightestRed: '#FF768E',
-        lightRed: '#FF5761',
-     red: '#E4163A',
-     darkRed: '#C60E2E',
-        lightestGrey: '#F6F6F6',
-     lightGrey: '#D1D1D1',
-     grey: '#9C9C9C',
-     middleGrey: '#707070',
-     darkGrey: '#303030',
-     white: '#FFFFFF',
-    },
-    sizes: {
+  colors: {
+    blue: '#344472',
+    lightestRed: '#FF768E',
+    lightRed: '#FF5761',
+    red: '#E4163A',
+    darkRed: '#C60E2E',
+    lightestGrey: '#F6F6F6',
+    lightGrey: '#D1D1D1',
+    grey: '#9C9C9C',
+    middleGrey: '#707070',
+    darkGrey: '#303030',
+    white: '#FFFFFF',
+    yellow: '#D9AD3A',
+  },
+  sizes: {
     buttonsSizes: {
-        small: "8px 25px",
-        middle: "8px 61px",
-        large: "10px 142px",
-    }
+      small: '8px 25px',
+      middle: '8px 61px',
+      large: '10px 142px',
+    },
   },
 };
