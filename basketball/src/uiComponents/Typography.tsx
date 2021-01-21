f9b2d7c763ad721305e9136ab351d1248080bafa
kross97 <kross78080@yandex.ru>
2021-01-21 18:43:52 +0300

    Add Typography

--- basketball/src/uiComponents/Typography.tsx
+++ basketball/src/uiComponents/Typography.tsx
@@ -1,7 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const TextStandart = styled.span`
-  font-family: Avenir sans-serif;
+  font-family: Avenir, sans-serif;
   font-style: normal;
   font-weight: 500;
   font-size: 16px;
@@ -9,5 +9,18 @@ export const TextStandart = styled.span`
 `;
 
 export const TextSmall = styled(TextStandart)`
-font-size: 14px;
+  font-size: 14px;
+`;
+
+export const TextExtraSmall = styled(TextStandart)`
+ font-size: 12px;
+`;
+
+export const TextLogo = styled.span`
+  font-family: Orbitron, sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 45px;
+  text-transform: uppercase;
 `;
