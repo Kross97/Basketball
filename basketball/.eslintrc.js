95219d13c46d978783947c9d93a04fde85d31431
kross97 <kross78080@yandex.ru>
2021-01-21 18:41:20 +0300

    Upgrade config

---
+++ basketball/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+        project: "./tsconfig.json",
+    },
+    plugins: [
+        '@typescript-eslint',
+        'react-hooks',
+    ],
+    extends: [
+        'airbnb-typescript',
+    ],
+    rules: {
+        "no-return-assign": 0,
+        "consistent-return": 0,
+        "import/prefer-default-export": 0,
+        "jsx-a11y/label-has-associated-control": 0,
+        "eqeqeq": 0,
+        "class-methods-use-this": 0,
+        "import/no-cycle": 0,
+        "no-case-declarations": 0,
+        "@typescript-eslint/no-use-before-define": 0,
+        "react/require-default-props": 0,
+        "react/no-unused-prop-types": 0,
+        "react/prop-types": 0,
+        "indent": "off",
+        "@typescript-eslint/indent": ["error"],
+        "import/no-extraneous-dependencies": 0,
+    },
+};
