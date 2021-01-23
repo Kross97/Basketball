5740663eada1213d9180700cf86612a828c5bb2a
Kross97 <kross78080@yandex.ru>
2021-01-23 21:30:15 +0200

    Add interfaces Player and Team

---
+++ basketball/src/helpers/interfaces/Player.ts
@@ -0,0 +1,11 @@
+export interface Player {
+  'name': string,
+  'number': number,
+  'position': string,
+  'team': number,
+  'birthday': string,
+  'height': number,
+  'weight': number,
+  'avatarUrl': string,
+  'id': number
+}
