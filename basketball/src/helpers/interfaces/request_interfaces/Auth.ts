be1dd887ffce15380dff50181a1a38bccafd6dea
Kross97 <kross78080@yandex.ru>
2021-01-24 17:33:29 +0200

    Add interfaces for requests

---
+++ basketball/src/helpers/interfaces/request_interfaces/Auth.ts
@@ -0,0 +1,15 @@
+export interface RegisterUser {
+  'userName': string,
+  'login': string,
+  'password': string
+}
+
+export interface UserChange {
+  'userName': string,
+  'avatarUrl': string
+}
+
+export interface LoginUser {
+  'login': string,
+  'password': string
+}
---
+++ basketball/src/helpers/interfaces/request_interfaces/Team.ts
@@ -0,0 +1,16 @@
+export interface Team {
+  'name': string,
+  'foundationYear': number,
+  'division': string,
+  'conference': string,
+  'imageUrl': string,
+}
+
+export interface TeamUpdate {
+  'name': string,
+  'foundationYear': number,
+  'division': string,
+  'conference': string,
+  'imageUrl': string,
+  'id': 0
+}
