0f5411574c29acb621ce8209af42b2c9b1391097
Kross97 <kross78080@yandex.ru>
2021-01-25 22:23:00 +0200

    Add actions errorSignIn and errorSingUp

--- basketball/src/store/index.ts
+++ basketball/src/store/index.ts
@@ -10,14 +10,18 @@ export const authDataUser = createSlice({
     authErrorMessageSignUp: '',
     authErrorMessageSignIn: '',
     localUserData: {
-      login: null,
-      password: null,
+      login: '',
+      password: '',
     },
   },
   reducers: {
     addAuthData: (state, action: any) => ({ ...state, authData: action.payload.authData }),
-    addAuthErrorSignUp: (state, action:any) => ({ ...state, authErrorMessageSignUp: `${action.payload.authError[0]}` }),
-    addAuthErrorSignIn: (state, action:any) => ({ ...state, authErrorMessageSignIn: `${action.payload.authError[0]}` }),
+    addAuthErrorSignUp: (state, action:any) => (
+      { ...state, authErrorMessageSignUp: action.payload.errorSignUp }
+    ),
+    addAuthErrorSignIn: (state, action:any) => (
+      { ...state, authErrorMessageSignIn: action.payload.errorSignIn }
+    ),
     addLocalUserData: (state, action: any) => (
       { ...state, localUserData: action.payload.userData }
     ),
