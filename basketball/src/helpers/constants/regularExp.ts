f9a23d247d6fd6012051be2a3e1aff58dc331620
kross97 <kross78080@yandex.ru>
2021-02-03 11:56:18 +0300

    fix regExp name for latin. and kiril words

--- basketball/src/helpers/constants/regularExp.ts
+++ basketball/src/helpers/constants/regularExp.ts
@@ -1,12 +1,12 @@
 export const regExpImageTeam = /^(http:\/\/)dev\.trainee/;
 
-export const regExpName = /^([^\W\d_]{3,})([^\W\d_] | \s)?([^\W\d_]{3,})$/i;
+export const regExpName = /^([A-ZА-Я]{3,13})\s?([A-ZА-Я]{3,13})$/i;
 
 export const regExpCommandName = /^([^\W\d_]{3,})([^\W\d_] | \s)?([^\W\d_]{3,})?([^\W\d_] | \s)?([^\W\d_]{3,})$/i;
 
 export const regExpDivision = /^([^\W\d]{3,})([^\W\d] | \s)?([^\W\d]{3,})?([^\W\d] | \s)?([^\W\d]{3,})$/;
 
-export const regExpLogin = /^([^\W\s]+)$/;
+export const regExpLogin = /^([^\W\s]{3,13})$/;
 
 export const regExpPassword = /^([^\s]+)$/i;
 
