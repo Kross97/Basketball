72e6441056072957094ed316b2c3fd3d68b6d0ae
Kross97 <kross78080@yandex.ru>
2021-02-03 20:12:07 +0200

    remove pirsist in home

--- basketball/src/index.tsx
+++ basketball/src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { PersistGate } from 'redux-persist/integration/react';
 import { configureStore } from '@reduxjs/toolkit';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
@@ -14,28 +11,18 @@ import { reducer } from './store';
 import reportWebVitals from './reportWebVitals';
 import './i18next';
 
-const persistConfig = {
-  key: 'root-basket',
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer,
 });
-
-const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
-        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
